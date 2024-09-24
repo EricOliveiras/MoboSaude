8c403ef3e7e10e8af8d0fbd2332b1d39e821cba9
Eric Nascimento <oiericoi@hotmail.com>
2024-09-24 20:28:32 -0300

    refactor: update App

--- App.tsx
+++ App.tsx
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet} from 'react-native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import FrontPage from './pages/frontPage';
+import Form from "./pages/form";
+
+export type RootStackParamList = {
+    Form: React.JSX.Element;
+    FrontPage: React.JSX.Element;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="FrontPage">
+                <Stack.Screen
+                    name="FrontPage"
+                    component={FrontPage}
+                    options={{headerShown: false}}
+                />
+                <Stack.Screen
+                    name="Form"
+                    component={Form}
+                    options={{
+                        headerShown: true,
+                        title: "Digite seus dados"
+                    }}
+                />
+            </Stack.Navigator>
+            <StatusBar style="auto"/>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
